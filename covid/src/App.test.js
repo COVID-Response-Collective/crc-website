2b156f4337d27cdba10c0b68f8ff50ad9ad1ca48
BurlapRobot <michael.hl.lee@gmail.com>
2020-03-19 23:02:41 -0700

    Initial add from Create React App

---
+++ covid/src/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders join discord link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Join The Discord/i);
+  expect(linkElement).toBeInTheDocument();
+});
